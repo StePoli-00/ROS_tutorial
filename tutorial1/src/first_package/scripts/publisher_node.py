15d691e310ae309c966eec2be6be139b166db2e5
Stefano <poli14.sp@gmail.com>
2024-06-21 17:15:28 +0200

    create custom message

--- tutorial1/src/first_package/scripts/publisher_node.py
+++ tutorial1/src/first_package/scripts/publisher_node.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env
 import rospy 
 from std_msgs.msg import String 
-
+from first_package.msg import position
 
 def talk_to_me():
     #1:name of topic #2: type of message #3: queue size of messages
-    pub=rospy.Publisher("talking_topic",String , queue_size=10) 
+    # pub=rospy.Publisher("talking_topic",String , queue_size=10) 
+    pub=rospy.Publisher("talking_topic",position , queue_size=10)
     #initialize node
     #1 name of node, create unique numer for the node
     rospy.init_node("publisher_node",anonymous=True)
@@ -14,7 +15,11 @@ def talk_to_me():
     rospy.loginfo("Publisher node started, now publishing messages")
     #until the node is running
     while not rospy.is_shutdown():
-        msg="Hello Stefano %s" % rospy.get_time()
+        # msg="Hello Stefano %s" % rospy.get_time()
+        msg=position()
+        msg.message="My position is: "
+        msg.x=2.0
+        msg.y=1.5
         #publish the message
         pub.publish(msg)
         rate.sleep()
--- tutorial1/src/first_package/scripts/subscriber_node.py
+++ tutorial1/src/first_package/scripts/subscriber_node.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env
 import rospy 
 from std_msgs.msg import String 
+from first_package.msg import position
 
 #String obj contains the data inside obj data
 def callback(data):
-    rospy.loginfo("Received data: %s",data.data)
+    # rospy.loginfo("Received data: %s",data.data)
+    rospy.loginfo("%s X: %f Y: %f",data.message,data.x,data.y)
 
 def listener():
-   rospy.init_node("Subscriber_Node",anonymous=True)
-   #1:topic to subscribe, 2:type, 3:function to call when a message arrive into topic
-   rospy.Subscriber("talking_topic",String,callback)
-   #allow to run the node until will be killed
-   rospy.spin()
-   return 
+    rospy.init_node("Subscriber_Node",anonymous=True)
+    #1:topic to subscribe, 2:type, 3:function to call when a message arrive into topic
+    #rospy.Subscriber("talking_topic",String,callback)
+    rospy.Subscriber("talking_topic",position,callback)
+    #allow to run the node until will be killed
+    rospy.spin()
+    return 
 
 
 if __name__=="__main__":
